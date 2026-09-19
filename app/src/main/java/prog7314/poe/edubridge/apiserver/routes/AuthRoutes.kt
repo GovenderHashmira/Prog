0ef9cb35ee583add789e78bf295c9f5207093108
Khumo Machoga ST10396677 <st10396677@imconnect.edu.za>
2026-09-19 20:52:55 +0200

    Updated AuthRoutes with correct imports

--- app/src/main/java/prog7314/poe/edubridge/apiserver/routes/AuthRoutes.kt
+++ app/src/main/java/prog7314/poe/edubridge/apiserver/routes/AuthRoutes.kt
@@ -6,6 +6,8 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import prog7314.poe.edubridge.apiserver.models.*
+import prog7314.poe.edubridge.apiserver.data.*
 
 // Authentication endpoints
 // POST / api/auth/login - authenticate and receive token
